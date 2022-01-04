e41a3b4deb2f512914058c021a44108d39a8d0c7
KinanLak <kinan.lakh@gmail.com>
2022-01-05 00:52:30 +0100

    partial main.c for playerPlacements test. NE PAS MERGE AVEC BRANCHE MAIN

--- src/draft/draft.c
+++ src/draft/draft.c
@@ -26,11 +26,11 @@ typedef struct t_joueur {
     int avatar;          // Hexadecimal code for the avatar selection
 } joueur;
 
-typedef struct t_case {
+typedef struct t_box {
     int id;
-    char nom[20];
     int x;
     int y;
+    char nom[20];
 } box;
 
 typedef struct t_terrain {
@@ -56,6 +56,213 @@ typedef struct t_terrain {
     bool hypotheque;   // True si la case est hypothéquée
 } terrain;
 
+void clearScreen() {  // permet de clear la console
+    system("cls");
+}
+
+void Color(int couleurDuTexte, int couleurDeFond) {  // fonction d'affichage de couleurs
+    HANDLE H = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(H, couleurDeFond * 16 + couleurDuTexte);
+}
+
+void gotoligcol(int lig, int col) {
+    COORD mycoord;
+    mycoord.X = col;
+    mycoord.Y = lig;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), mycoord);
+}
+
+void showLogo() {
+    gotoligcol(0, 0);
+    printf(
+        "MONO            POLY  	     MONOPOLY	      MONO	  PO   	     "
+        "MONOPOLY         MONOPOLYMONO           MONOPOLY         MO          MO "
+        "       NO");
+    printf(
+        "\nPOLYMONO    MONOPOLY	    NO      MO        NOPO        NO        NO "
+        "     MO        NO         PO         NO      MO        NO           NO  "
+        "    MO");
+    printf(
+        "\nMONOPOLY    POLYMONO	   PO	     NO       PO  MO      MO       PO  "
+        "      NO       PO          LY       PO        NO       PO            PO "
+        "   LY");
+    printf(
+        "\nPOLY	MONO	POLY	  LY	      PO      LY  NO      LY      LY   "
+        "       PO      LY          MO      LY          PO      LY             "
+        "LY  PO");
+    printf(
+        "\nMONO	POLY	MONO     MO            LY     MO    PO    PO     MO    "
+        "        LY     MO         NO      MO            LY     MO              "
+        "MONO");
+    printf(
+        "\nPOLY		POLY	NO		MO    NO    LY	  NO    NO     "
+        "         MO    NOPONOMOLYPO      NO              MO    NO               "
+        "NO");
+    printf(
+        "\nMONO		MONO	 PO	       NO     PO      MO  MO     PO    "
+        "        NO     PO                 PO            NO     PO               "
+        "PO");
+    printf(
+        "\nPOLY		POLY	  LY	      PO      LY      NO  LY      LY   "
+        "       PO      LY                  LY          PO      LY               "
+        "LY");
+    printf(
+        "\nMONO		MONO	   MO	     LY       MO        POPO       MO  "
+        "      LY       MO                   MO        LY       MO               "
+        "MO");
+    printf(
+        "\nPOLY		POLY 	    NO      MO        NO        LYNO        NO "
+        "     MO        NO                    NO      MO        NO               "
+        "NO");
+    printf(
+        "\nMONO		POLY	     POLYMONO         PO          MO         "
+        "POLYMONO         PO                     POLYMONO         POLYMONOPOLY   "
+        "  LY");
+    gotoligcol(14, 5);
+}
+
+void display() {
+    clearScreen();
+    showLogo();
+}
+
+
+
+int choixAvatar(int nbJoueurs, int currentPlayer) {
+    bool end = false;
+    int av;
+    int key, c;
+    int avatar[10] = {0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x0B, 0x0C, 0x0E, 0x0F};
+    int selection = 0;
+    display();
+    printf("Veuillez choisir votre avatar");
+    gotoligcol(17, 7);
+    printf("<---     --->");
+    gotoligcol(17, 13);
+    printf("%c", avatar[selection]);
+    while (!end) {
+        gotoligcol(17, 13);
+        if (kbhit()) {
+            key = getch();
+            switch (key) {
+                case 75:
+                    if (selection > 0) {
+                        selection = selection - 1;
+                    }
+                    printf("%c", avatar[selection]);
+                    break;
+
+                case 77:
+                    if (selection < 9) {
+                        selection++;
+                    }
+                    printf("%c", avatar[selection]);
+                    break;
+                case 13:
+                    end = true;
+                    break;
+                    /* Pour vérifier si l'avat est déjà pris
+                    av = avatar[selection];
+                    switch (currentPlayer) {
+                        case 1:
+                            if ((av == j2.avatar) || (av == j3.avatar) || (av == j4.avatar)) {
+                                printf("Avatar deja choisi !");
+                            } else {
+                                end = true;
+                            }
+                            break;
+                        case 2:
+                            if ((av == j1.avatar) || (av == j3.avatar) || (av == j4.avatar)) {
+                                printf("Avatar deja choisi !");
+                            } else {
+                                end = true;
+                            }
+                            break;
+                        case 3:
+                            if ((av == j1.avatar) || (av == j2.avatar) || (av == j4.avatar)) {
+                                printf("Avatar deja choisi !");
+                            } else {
+                                end = true;
+                            }
+                            break;
+                        case 4:
+                            if ((av == j1.avatar) || (av == j2.avatar) || (av == j3.avatar)) {
+                                printf("Avatar deja choisi !");
+                            } else {
+                                end = true;
+                            }
+                            break;
+                        {*/
+            }
+        }
+    }
+
+    gotoligcol(18, 5);
+    printf("Selection enregistree");
+    //animation(20, 0, 50, 28);
+
+    /*
+    for (c = selection - 1; c < 10 - 1; c++)
+        avatar[c] = avatar[c + 1];
+
+    printf("Resultant array:\n");
+
+    for (c = 0; c < 10 - 1; c++)
+        printf("%c\n", avatar[c]);
+    */
+
+    return avatar[selection];
+}
+
+void home() {  // menu principal du jeu
+    int choice = 0;
+
+    showLogo();
+
+    gotoligcol(14, 23);
+    Color(15, 2);
+    printf("1-Lancer une nouvelle partie");
+    gotoligcol(14, 60);
+    Color(15, 5);
+    printf("2-Sauvegarder la partie");
+    gotoligcol(14, 93);
+    Color(15, 4);
+    printf("3-Charger une ancienne partie");
+    gotoligcol(16, 33);
+    Color(15, 9);
+    printf("4-Consulter les regles");
+    gotoligcol(16, 67);
+    Color(15, 11);
+    printf("5-Credits");
+    gotoligcol(16, 89);
+    Color(15, 8);
+    printf("6-Quitter la partie");
+
+    gotoligcol(19, 0);
+    Color(15, 0);
+    printf("--> Que choisissez-vous de faire ? Tapez un chiffre : ");
+    scanf("%d", &choice);
+
+    while (choice < 1 || choice > 6) {
+        printf("Votre saisie (%d) n'est pas valide. Veuillez entrer un chiffre a nouveau : \n", choice);
+        scanf("%d", &choice);
+    }
+    switch (choice) {
+        case 1:
+            clearScreen();
+            //newGame();
+            break;
+        case 4:
+            clearScreen();
+            //regles();
+            break;
+        case 5:
+            clearScreen();
+            //credits();
+            break;
+    }
+}
+
 joueur *creationDesJoueurs(int nombreDeJoueurs) {
     int emptyCard[10];
     int emptyField[26];
@@ -73,13 +280,13 @@ joueur *creationDesJoueurs(int nombreDeJoueurs) {
     listeJoueurs[3] = tempJ4;
 
     for (int i = 0; i < nombreDeJoueurs; i++) {
-        // display();
+        display();
         printf("Entrez le nom du joueur %d : ", i + 1);
         scanf("%s", listeJoueurs[i].username);
-        // listeJoueurs[i].avatar = choixAvatar(nombreDeJoueurs, 1);
+        listeJoueurs[i].avatar = choixAvatar(nombreDeJoueurs, 1);
     }
 
-    // display();
+    display();
     gotoligcol(14, 0);
 
     for (int i = 0; i < nombreDeJoueurs; i++) {
@@ -88,46 +295,47 @@ joueur *creationDesJoueurs(int nombreDeJoueurs) {
 
     printf("\nChargement de la partie en cours");
 
-    // animation(20, 0, 75, 50);
+    //animation(20, 0, 75, 50);
 
     return listeJoueurs;
 }
 
-void gotoligcol(int lig, int col) {
-    COORD mycoord;
-    mycoord.X = col;
-    mycoord.Y = lig;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), mycoord);
-}
 
-box *creationCase() {
+
+box *creationBox() {
     
     box *listeCase = (box *)malloc(40 * sizeof(box));
+
+    char *name[20];
 
 
     for (int i = 0; i < 22; i++) {
         FILE *texte = NULL;
         char ignore[1024];
-        int donnee[4];
+        int donnee[3];
 
         listeCase[40];
 
-        texte = fopen("fields.txt", "r");
+        texte = fopen("board.txt", "r");
         if (texte == NULL) {
             printf("Error: Cannot open");
         }
         for (int j = 0; j < i; j++) {
             fgets(ignore, sizeof(ignore), texte);
         }
-        fscanf(texte, "%d;%s;%d;%d", &donnee[0], &donnee[1], &donnee[2], &donnee[3]);
+        fscanf(texte, "%d;%s;%d;%d", &donnee[0], &name, &donnee[1], &donnee[2]);
 
-        box tempBox = {donnee[0], donnee[1], donnee[2], donnee[3]};
+        box tempBox = {donnee[0], donnee[1], donnee[2], (char) i};
+
+        //box tempBox.id = donnee[0];
 
         listeCase[i] = tempBox;
     }
 
     return listeCase;
 }
+
+
 
 terrain *creationTerrain() {  // création d'une instance (un album)
     terrain *listeTerrain = (terrain *)malloc(23 * sizeof(terrain));
@@ -139,13 +347,13 @@ terrain *creationTerrain() {  // création d'une instance (un album)
                                  "After Hours", "Thriller", "DLL", "Trinity", "JVLIVS", "Ipseite", "Cyborg"};
 
     for (int i = 0; i < 22; i++) {
-        char *nomCurrent = listeNomTerrain[i];
+        //char *nomCurrent = listeNomTerrain[i];
 
         FILE *texte = NULL;
         char ignore[1024];
         int donnee[14];
         char proprio[10];
-        texte = fopen("data/fields.txt", "r");
+        texte = fopen("fields.txt", "r");
         if (texte == NULL) {
             printf("Error: Cannot open");
         }
@@ -156,7 +364,7 @@ terrain *creationTerrain() {  // création d'une instance (un album)
                &donnee[2], &donnee[3], &donnee[4], &donnee[5], &donnee[6], &donnee[7],
                &donnee[8], &donnee[9], &donnee[10], &donnee[11], &donnee[12], &donnee[13]);
 
-        instance.nom = nomCurrent;
+        instance.nom = listeNomTerrain[i];
         instance.id = donnee[0];
         instance.defaultPrice = donnee[1];
         instance.idOnBoard = donnee[14];
@@ -182,8 +390,10 @@ terrain *creationTerrain() {  // création d'une instance (un album)
     return listeTerrain;
 }
 
+
+
 void afficherJoueurPlateau(joueur *joueurs, terrain *terrains) {
-    int posJoueur[4] = {5, 5, 12, 12};
+    int posJoueur[4] = {0};
 
     /*for (int i = 0; i < 4; i++) {
         posJoueur[i] = joueurs[i].position;
@@ -210,11 +420,13 @@ void afficherJoueurPlateau(joueur *joueurs, terrain *terrains) {
             }
         }
     }
+    /*
     printf("|ID|Position|Nombre|\n");
     printf("| 1|      %d|    %d|\n", pos1, nbPos1);
     printf("| 2|      %d|    %d|\n", pos2, nbPos2);
     printf("| 3|      %d|    %d|\n", pos3, nbPos3);
     printf("| 4|      %d|    %d|\n\n", pos4, nbPos4);
+    */
 
     if (pos1 == pos2) {
         nbPos2 = nbPos1;
@@ -235,42 +447,47 @@ void afficherJoueurPlateau(joueur *joueurs, terrain *terrains) {
         nbPos4 = nbPos3;
     }
 
+    /*
     printf("|ID|Position|Nombre|\n");
     printf("| 1|      %d|    %d|\n", pos1, nbPos1);
     printf("| 2|      %d|    %d|\n", pos2, nbPos2);
     printf("| 3|      %d|    %d|\n", pos3, nbPos3);
     printf("| 4|      %d|    %d|\n\n", pos4, nbPos4);
-
+    */
     int posList[4] = {pos1, pos2, pos3, pos4};
     int nbPosList[4] = {nbPos1, nbPos2, nbPos3, nbPos4};
-
+    
     for (int i = 0; i < 4; i++) {
         if (nbPosList[i] == 1) {
-            gotoligcol(terrains[posList[i]].x + 5, terrains[posList[i]].y + 2);
+            //printf("ouodauo");
+            gotoligcol(terrains[posList[i]].y + 2,terrains [posList[i]].x + 5);
             printf("%c", joueurs[i].avatar);
         } else if (nbPosList[i] == 2) {
-            gotoligcol(terrains[posList[i]].x + 4, terrains[posList[i]].y + 2);
+            //printf("dazed");
+            gotoligcol(terrains[posList[i]].y + 2, terrains [posList[i]].x + 4);
             printf("%c", joueurs[i].avatar);
-            gotoligcol(terrains[posList[i]].x + 7, terrains[posList[i]].y + 2);
+            gotoligcol(terrains[posList[i]].y + 2, terrains[posList[i]].x + 7);
             printf("%c", joueurs[i + 1].avatar);
             i++;
         } else if (nbPosList[i] == 3) {
-            gotoligcol(terrains[posList[i]].x + 3, terrains[posList[i]].y + 2);
+            //printf("fesfs");
+            gotoligcol(terrains[posList[i]].y + 2, terrains[posList[i]].x + 3);
             printf("%c", joueurs[i].avatar);
-            gotoligcol(terrains[posList[i]].x + 5, terrains[posList[i]].y + 2);
+            gotoligcol(terrains[posList[i]].y + 2, terrains[posList[i]].x + 5);
             printf("%c", joueurs[i + 1].avatar);
-            gotoligcol(terrains[posList[i]].x + 7, terrains[posList[i]].y + 2);
+            gotoligcol(terrains[posList[i]].y + 2, terrains[posList[i]].x + 7);
             printf("%c", joueurs[i + 2].avatar);
             i++;
             i++;
         } else if (nbPosList[i] == 4) {
-            gotoligcol(terrains[posList[i]].x + 2, terrains[posList[i]].y + 2);
+            //printf("fsedf");
+            gotoligcol(terrains[posList[i]].y + 2, terrains[posList[i]].x + 2);
             printf("%c", joueurs[i].avatar);
-            gotoligcol(terrains[posList[i]].x + 4, terrains[posList[i]].y + 2);
+            gotoligcol(terrains[posList[i]].y + 2, terrains[posList[i]].x + 4);
             printf("%c", joueurs[i + 1].avatar);
-            gotoligcol(terrains[posList[i]].x + 6, terrains[posList[i]].y + 2);
+            gotoligcol(terrains[posList[i]].y + 2, terrains[posList[i]].x + 6);
             printf("%c", joueurs[i + 2].avatar);
-            gotoligcol(terrains[posList[i]].x + 8, terrains[posList[i]].y + 2);
+            gotoligcol(terrains[posList[i]].y + 2, terrains[posList[i]].x + 8);
             printf("%c", joueurs[i + 3].avatar);
             i++;
             i++;
@@ -279,9 +496,121 @@ void afficherJoueurPlateau(joueur *joueurs, terrain *terrains) {
     }
 }
 
+void creationCase(char titre[15], int x, int y, int id, int couleur) {
+    /*
+    0 : Noir
+    1 : Bleu foncé
+    2 : Vert foncé
+    3 : Turquoise
+    4 : Rouge foncé
+    5 : Violet
+    6 : Vert caca d'oie
+    7 : Gris clair
+    8 : Gris foncé
+    9 : Bleu fluo
+    10 : Vert fluo
+    11 : Turquoise
+    12 : Rouge fluo
+    13 : Violet 2
+    14 : Jaune
+    15 : Blanc
+    */
+
+    int longueur = 0;
+    int bord = 0;
+    longueur = strlen(titre);
+    bord = (13 - longueur) / 2;
+    Color(0, couleur);
+    gotoligcol(x, y);
+    if (id == 0) {
+        for (int j = 0; j < 13; j++) {
+            printf(" ");
+        }
+    } else {
+        if (id < 10) {
+            for (int i = 0; i < 6; i++) {
+                printf(" ");
+            }
+            printf("%d", id);
+            for (int a = 0; a < 6; a++) {
+                printf(" ");
+            }
+        } else {
+            for (int i = 0; i < 5; i++) {
+                printf(" ");
+            }
+            printf("%d", id);
+            for (int a = 0; a < 6; a++) {
+                printf(" ");
+            }
+        }
+    }
+    printf("\n");
+    gotoligcol(x + 1, y);
+    for (int k = 0; k < bord; k++) {
+        printf(" ");
+    }
+    gotoligcol(x + 1, y + bord);
+    printf(titre);
+    gotoligcol(x + 1, y + bord + longueur);
+    for (int z = y + bord + longueur; z < y + 13; z++) {
+        printf(" ");
+    }
+    for (int n = 0; n < 3; n++) {
+        gotoligcol(x + 2 + n, y);
+        for (int j = 0; j < 13; j++) {
+            printf(" ");
+        }
+        printf("\n");
+    }
+    Color(15, 0);
+}
+
+void plateauGraphique(terrain *listeTerrains) {  // création du plateau de base, il reste inchangé après
+
+    int couleurCaseNeutre = 15;
+
+    for (int i = 0; i < 22; i++) {
+        terrain currentTerrain = listeTerrains[i];
+        char *nomCurrent = currentTerrain.nom;
+        creationCase(nomCurrent, currentTerrain.x, currentTerrain.y, currentTerrain.id, currentTerrain.couleur);
+    }
+    creationCase("Soundcloud", 0, 0, 0, couleurCaseNeutre);
+    creationCase("Communaute", 15, 0, 0, couleurCaseNeutre);
+    creationCase("Zenith", 25, 0, 0, couleurCaseNeutre);
+    creationCase("Sacem", 40, 0, 0, couleurCaseNeutre);
+    creationCase("Finito", 50, 0, 0, couleurCaseNeutre);
+    creationCase("Chance", 0, 26, 0, couleurCaseNeutre);
+    creationCase("Zenith", 0, 65, 0, couleurCaseNeutre);
+    creationCase("Sacem", 0, 104, 0, couleurCaseNeutre);
+    creationCase("Drama", 0, 130, 0, couleurCaseNeutre);
+    creationCase("Communaute", 15, 130, 0, couleurCaseNeutre);
+    creationCase("Zenith", 25, 130, 0, couleurCaseNeutre);
+    creationCase("Chance", 30, 130, 0, couleurCaseNeutre);
+    creationCase("Sacem", 40, 130, 0, couleurCaseNeutre);
+    creationCase("DEPART", 50, 130, 0, couleurCaseNeutre);
+    creationCase("Chance", 50, 39, 0, couleurCaseNeutre);
+    creationCase("Zenith", 50, 65, 0, couleurCaseNeutre);
+    creationCase("Sacem", 50, 78, 0, couleurCaseNeutre);
+    creationCase("Communaute", 50, 104, 0, couleurCaseNeutre);
+}
+
+
 int main() {
+    system("cls");
+
+
+
     joueur *pJoueurs = creationDesJoueurs(4);
     terrain *pTerrains = creationTerrain();
+    box *bList = creationBox();
 
-    box bList[40] = afficherJoueurPlateau(pJoueurs, pTerrains);
+    plateauGraphique(pTerrains);
+    afficherJoueurPlateau(pJoueurs, pTerrains);
+
+    
+    free(bList);
+    free(pJoueurs);
+    free(pTerrains);
+    gotoligcol(60, 0);
 }
